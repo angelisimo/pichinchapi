bcc18e0382f0c8b6561bdaa51f628013d953eea9
angelisimo <angeltorresmoreira@gmail.com>
2019-07-05 04:17:08 +0000

    first commit

---
+++ pichapi/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+
+class Api(models.Model):
+    Amessage = models.CharField(max_length=100, blank=True, default='')
+    Ato = models.CharField(max_length 
+    Afrom
+    Atimetolifesec
+
